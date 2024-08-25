f5b63c3b7c375253a611b77aee449347b42db79c
tonydc24 <124815616+tonydc24@users.noreply.github.com>
2024-08-25 16:54:35 -0600

    Tracking the ball

--- ProyectoVolleyball/Assets/Scripts/EnemyTracker.cs
+++ ProyectoVolleyball/Assets/Scripts/EnemyTracker.cs
@@ -1,21 +1,74 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyTracker : MonoBehaviour
 {
+    private int ANIMATION_SPEED;
+    private int ANIMATION_FORCE;
+    private int ANIMATION_FALL;
+
     [Header("Tracking")]
     [SerializeField]
     Transform ballTracking;
-    [Header("Move")]
+
+    [Header("Movement")]
+    [SerializeField]
+    float walkSpeed;
+
     [SerializeField]
     float jumpForce;
 
+    [SerializeField]
+    float gravityMultiplier;
+
+    [SerializeField]
+    Transform groundCheck;
+
+    [SerializeField]
+    Vector2 groundCheckSize;
+
+    [SerializeField]
+    LayerMask groundMask;
+
+
+    Rigidbody2D _rigidbody;
+    Animator _animator;
+
+    float _gravityY;
+    float _velocityY;
+
+    bool _isGrounded;
+    bool _isJumping;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponentInChildren<Animator>();
+
+        _gravityY = Physics2D.gravity.y;
+
+        ANIMATION_SPEED = Animator.StringToHash("speed");
+        ANIMATION_FORCE = Animator.StringToHash("force");
+        ANIMATION_FALL = Animator.StringToHash("fall");
+    }
+
+    private void Start()
+    {
+        HandleGrounded();
+    }
+
     private void Update()
+    {
+        HandleGravity();
+        
+    }
+    private void FixedUpdate()
     {
         AiController();
     }
-    void AiController()
+    private void AiController()
     {
         Vector3 tmp = ballTracking.position;
 
@@ -23,42 +76,122 @@ public class EnemyTracker : MonoBehaviour
         {
             if (tmp.x > transform.position.x)
             {
-               // MoveRight();
+                MoveRight();
             }
             else if (tmp.x < transform.position.x)
             {
-               // MoveLeft();
+                MoveLeft();
             }
 
             if (Mathf.Abs(tmp.y - transform.position.y) > 1.5f &&
                 Mathf.Abs(tmp.y - transform.position.y) < 4f && tmp.x < 1.75f)
             {
-               // Jump();
+                Debug.Log("Estoy saltando");
+                Debug.Log($"IsGrounded: {_isGrounded}, VelocityY: {_velocityY}");
+                Jump();
             }
         }
         else if ((tmp.x > 0 && Mathf.Abs(tmp.x - transform.position.x) <= 0.3f) || (tmp.x <= 0 && tmp.x >= -0.6f))
         {
-            //Stay();
+            Stay();
         }
         else if (tmp.x < -0.6f)
         {
             if (transform.position.x < 3.75f)
             {
-              //  MoveRight();
+                MoveRight();
             }
             else if (transform.position.x > 4.1f)
             {
-               // MoveLeft();
+                MoveLeft();
             }
             else
             {
-              //  Stay();
+                Stay();
             }
         }
         else
         {
-           // Stay();
+            Stay();
         }
+    }
+    private void HandleGravity()
+    {
+        if (_isGrounded)
+        {
+            if (_velocityY < -1.0F)
+            {
+                _velocityY = -1.0F;
+            }
+        }
+    }
+
+    private void HandleGrounded()
+    {
+        _isGrounded = IsGrounded();
+        if (!_isGrounded)
+        {
+            StartCoroutine(WaitForGroundedCoroutine());
+        }
+    }
+
+    public void Jump()
+    {
+        if (_isGrounded)
+        {
+            _isGrounded = false;
+            _isJumping = true;
+
+            _velocityY = jumpForce;
+            _animator.SetTrigger(ANIMATION_FORCE);
+            StartCoroutine(WaitForGroundedCoroutine());
+        }
+    }
+    private void MoveLeft()
+    {
+        float speed = 1.0F;
+        _animator.SetFloat(ANIMATION_SPEED, speed);
+
+        Vector2 velocity = new Vector2(-1.0F, 0.0F) * walkSpeed * Time.fixedDeltaTime;
+        velocity.y = _velocityY;
+
+        _rigidbody.velocity = velocity;
+    }
+
+    private void MoveRight()
+    {
+        float speed = 1.0F;
+        _animator.SetFloat(ANIMATION_SPEED, speed);
+
+        Vector2 velocity = new Vector2(1.0F, 0.0F) * walkSpeed * Time.fixedDeltaTime;
+        velocity.y = _velocityY;
+
+        _rigidbody.velocity = velocity;
+    }
+    private void Stay()
+    {
+        _rigidbody.velocity = new Vector2(0.0f, _rigidbody.velocity.y);
+
+        _animator.SetFloat(ANIMATION_SPEED, 0.0f);
+    }
+  
+
+    private bool IsGrounded()
+    {
+        Collider2D collider2D = Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0.0F, groundMask);
+        return collider2D != null;
+    }
+
+    private IEnumerator WaitForGroundedCoroutine()
+    {
+        yield return new WaitUntil(() => !IsGrounded());
+        yield return new WaitUntil(() => IsGrounded());
+        _isGrounded = true;
+    }
+
+    private void Smash()
+    {
+    
     }
 
 }
