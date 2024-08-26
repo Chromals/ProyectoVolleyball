af6a6800c026bf06e4f3004466a6b618a913cedc
Juan Vargas <jjvarhdz@gmail.com>
2024-08-25 23:46:34 -0600

    Opciones cambian de color

--- ProyectoVolleyball/Assets/Scripts/MenuManager.cs
+++ ProyectoVolleyball/Assets/Scripts/MenuManager.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
-
+    
     public GameObject gameWelcomePanel;
     public GameObject gameConfigPanel;
 
-
+    
     public TMP_Text setsText;
     public TMP_Text pointsText;
     public TMP_Text timeText;
@@ -16,6 +17,15 @@ public class MenuManager : MonoBehaviour
     private int selectedSets = 3;
     private int selectedPoints = 15;
     private int selectedTime = 5;
+
+   
+    private Button selectedSetsButton;
+    private Button selectedPointsButton;
+    private Button selectedTimeButton;
+
+
+    public Sprite selectedButtonSprite;
+    private Sprite originalButtonSprite;
 
    
     public void OpenSettings()
@@ -28,12 +38,10 @@ public class MenuManager : MonoBehaviour
     {
         gameConfigPanel.SetActive(false);
         gameWelcomePanel.SetActive(true);
-
-        
         UpdateGameWelcomeTexts();
     }
 
-   
+  
     private void UpdateGameWelcomeTexts()
     {
         setsText.text = selectedSets.ToString();
@@ -41,23 +49,50 @@ public class MenuManager : MonoBehaviour
         timeText.text = selectedTime.ToString() + ":00";
     }
 
-    
+   
     public void SelectSets(int sets)
     {
         selectedSets = sets;
+       
+        UpdateButtonSelection(ref selectedSetsButton, UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.GetComponent<Button>());
     }
 
+    
     public void SelectPoints(int points)
     {
         selectedPoints = points;
+        
+        UpdateButtonSelection(ref selectedPointsButton, UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.GetComponent<Button>());
     }
 
+  
     public void SelectTime(int time)
     {
         selectedTime = time;
+       
+        UpdateButtonSelection(ref selectedTimeButton, UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.GetComponent<Button>());
     }
 
-   
+ 
+    private void UpdateButtonSelection(ref Button previousSelectedButton, Button newSelectedButton)
+    {
+        
+        if (previousSelectedButton != null)
+        {
+            previousSelectedButton.GetComponent<Image>().sprite = originalButtonSprite;
+        }
+
+       
+        previousSelectedButton = newSelectedButton;
+
+       
+        originalButtonSprite = newSelectedButton.GetComponent<Image>().sprite;
+
+     
+        newSelectedButton.GetComponent<Image>().sprite = selectedButtonSprite;
+    }
+
+  
     public void QuitGame()
     {
         Application.Quit();
